e3d703ca1b68b833967d4ae91318b5ea70cdf670
Yongyuth (Private) <yongyuth_2527@hotmail.com>
2018-01-31 22:18:36 +0700

    Revert "Create Project"
    This reverts commit c25234b2a25cd03d5c51287f4b2a3b421f2aea91.

---
+++ application/views/master/Car/car_edit.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<div class="page-header">
+  <h1>ข้อมูลรถขนส่ง <small>ข้อมูลรถขนส่งภายในบริษัท (ส่วนหัว/ส่วนหาง)</small></h1>
+</div>
+<div class="col-12">
+    <ol class="breadcrumb">
+        <li><a href="<?php echo base_url('Car/index'); ?> ">Home</a></li>
+        <li class="active">New Car</li>
+    </ol>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <div class="row">
+                <div class="col-xs-6 ">
+                    <h4>
+                    <?php
+                    echo 'ข้อมูลรถ';
+                    ?>
+                    </h4>
+                </div>
+                <div class="col-xs-6 text-right">
+                    <button type="button" class="btn btn-success"><i class="fa fa-newspaper-o" style="padding-right: 5px;"></i>Save</button>
+                </div>
+            </div>
+        </div>
+        <div class="panel-body">            
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+
+        </div>
+        <div class="panel-footer">Panel Footer</div>
+    </div>
+</div>
