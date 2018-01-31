86398377182767b1ffbbb9a165a6504f45d4d3cd
Yongyuth (Private) <yongyuth_2527@hotmail.com>
2018-01-31 22:55:31 +0700

    Create Project

--- application/views/master/index.php
+++ application/views/master/index.php
@@ -9,24 +9,24 @@
             body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
         </style>
     <body >
-        <div align="center">
-            <div class="col-12">
+        <div align="center" style="position: relative;">
+            <div class="col-xs-12 col-sm-8 col-md-6" style="position: absolute;margin: auto;left: 0px;right: 0px;">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <div class="row">
-                            <div class="col-xs-6 ">
+                            <div class="col-xs-12 ">
 
                                 <title>เข้าสู่ระบบ</title>
 
                                 <center><h2>เข้าสู่ระบบ</h2></center>
-                                <div class="col align-self-center">
+                                <div class="">
                                     <form action="home.php" enctype="multipart/form-data">
-                                        <div class="form-group">
+                                        <div class="form-group text-left">
                                             <label for="exampleInputEmail1">อีเมล</label>
                                             <input type="email" class="form-control" id="email" placeholder="ใส่อีเมล" value="<?php echo $txtemail; ?>">
 
                                         </div>
-                                        <div class="form-group">
+                                        <div class="form-group text-left">
                                             <label for="exampleInputPassword1">รหัสผ่าน</label>
                                             <input type="password" class="form-control" id="pword" placeholder="ใส่รหัสผ่าน">
                                         </div>
@@ -42,7 +42,6 @@
                                                 <div class="modal-dialog modal-sm">
                                                     <div class="modal-content">
                                                         <div class="modal-header">
-
                                                             <h4 class="modal-title">กรุณาใส่อีเมล</h4>
                                                         </div>
                                                         <div class="modal-body">
@@ -75,32 +74,32 @@
                 </div>
             </div>
         </div>
-            <script>
-                // Accordion 
-                function myAccFunc() {
-                    var x = document.getElementById("demoAcc");
-                    if (x.className.indexOf("w3-show") == -1) {
-                        x.className += " w3-show";
-                    } else {
-                        x.className = x.className.replace(" w3-show", "");
-                    }
+        <script>
+            // Accordion 
+            function myAccFunc() {
+                var x = document.getElementById("demoAcc");
+                if (x.className.indexOf("w3-show") == -1) {
+                    x.className += " w3-show";
+                } else {
+                    x.className = x.className.replace(" w3-show", "");
                 }
+            }
 
-                // Click on the "Jeans" link on page load to open the accordion for demo purposes
-                document.getElementById("myBtn").click();
+            // Click on the "Jeans" link on page load to open the accordion for demo purposes
+            document.getElementById("myBtn").click();
 
 
-                // Script to open and close sidebar
-                function w3_open() {
-                    document.getElementById("mySidebar").style.display = "block";
-                    document.getElementById("myOverlay").style.display = "block";
-                }
+            // Script to open and close sidebar
+            function w3_open() {
+                document.getElementById("mySidebar").style.display = "block";
+                document.getElementById("myOverlay").style.display = "block";
+            }
 
-                function w3_close() {
-                    document.getElementById("mySidebar").style.display = "none";
-                    document.getElementById("myOverlay").style.display = "none";
-                }
-            </script>
+            function w3_close() {
+                document.getElementById("mySidebar").style.display = "none";
+                document.getElementById("myOverlay").style.display = "none";
+            }
+        </script>
 
 
     </body>
