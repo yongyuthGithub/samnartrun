4439c016eed7de3573e38bb080e98b2b3e32e955
Yongyuth (Private) <yongyuth_2527@hotmail.com>
2018-02-06 00:22:48 +0700

    Add DataTable Bootstrap

--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -1,64 +1,71 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+//require_once __DIR__ . '\..\models\PCenter.php';
 require __DIR__ . '/../core/PCenter.php';
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Fule extends PCenter {
+class Menu extends PCenter {
 
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     * 		http://example.com/index.php/welcome
+     * 	- or -
+     * 		http://example.com/index.php/welcome/index
+     * 	- or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see https://codeigniter.com/user_guide/general/urls.html
+     */
     public function __construct() {
         parent::__construct();
 //        $this->load->helper('url');
         //$this->load-model('dbconnect');
     }
 
-    //**** Pump
     public function index() {
-        $data['page'] = 'master/Fule/fule_main';
-
-        $this->load->model('fule_model');
-        $this->fule_model->insert_into_fule();
-
+        $data['page'] = 'setting/menu/menu_main';
         $this->load->view('layout/nav', $data);
     }
 
-    public function edit() {
-
-        $this->load->view('master/Fule/fule_edit');
+    public function editMenuPage() {
+        $this->load->view('setting/menu/menu_edit');
     }
 
-    public function findPump() {
-        $query = $this->db->select('RowKey, Pump, PumpType, ')->from('MSTPump')->get();
-        $_array = array();
-        foreach ($query->result() as $row) {
-            $_ar = array(
-                'key' => $row->RowKey,
-                'Pump' => $row->Pump,
-                'PumpType' => intval($row->PumpType),
-            );
-            array_push($_array, $_ar);
-        }
-        echo json_encode($_array);
+    public function findMenu() {
+        $qryMenu = $this->db->select('RowKey AS key,'
+                        . ' Menu,'
+                        . ' Description')
+                ->get('USRMenu');
+        echo json_encode($qryMenu->result());
     }
 
-    public function editPump() {
+    public function editMenu() {
         $_data = json_decode($_POST['data']);
         $vReturn = (object) [];
 
         $this->db->trans_begin();
-
         if ($_data->RowKey === PCenter::GUID_EMPTY()) {
-            $queryChk = $this->db->where('Pump', $_data->Pump)->from('MSTPump')->count_all_results();
+            $queryChk = $this->db->where('Menu', $_data->Menu)->from('USRMenu')->count_all_results();
             if ($queryChk > 0) {
                 $vReturn->success = false;
                 $vReturn->message = 'This information is already in the system.';
             } else {
+                $quySeq = $this->db->select_max('Seq')->get('USRMenu');
+                $Seq = $quySeq->row();
+
                 $_data->RowKey = PCenter::GUID();
+                $_data->Seq = $Seq->Seq + 1;
                 $_data->RowStatus = true;
                 $_data->CreateBy = PCenter::GUID_EMPTY();
                 $_data->CreateDate = PCenter::DATATIME_DB(new DateTime());
                 $_data->UpdateBy = PCenter::GUID_EMPTY();
                 $_data->UpdateDate = PCenter::DATATIME_DB(new DateTime());
-                $this->db->insert('MSTPump', $_data);
+                $this->db->insert('USRMenu', $_data);
                 if ($this->db->trans_status() === FALSE) {
                     $this->db->trans_rollback();
                     $vReturn->success = false;
@@ -69,17 +76,17 @@ class Fule extends PCenter {
                 }
             }
         } else {
-            $queryChk = $this->db->where('Pump', $_data->Pump)->where('RowKey !=', $_data->RowKey)->from('MSTPump')->count_all_results();
+            $queryChk = $this->db->where('Menu', $_data->Menu)->where('RowKey !=', $_data->RowKey)->from('USRMenu')->count_all_results();
             if ($queryChk > 0) {
                 $vReturn->success = false;
                 $vReturn->message = 'This information is already in the system.';
             } else {
                 $update = (object) [];
-                $update->Pump = $_data->Pump;
-                $update->PumpType = $_data->PumpType;
+                $update->Menu = $_data->Menu;
+                $update->Description = $_data->Description;
                 $update->UpdateBy = PCenter::GUID_EMPTY();
                 $update->UpdateDate = PCenter::DATATIME_DB(new DateTime());
-                $this->db->where('RowKey', $_data->RowKey)->update('MSTPump', $update);
+                $this->db->where('RowKey', $_data->RowKey)->update('USRMenu', $update);
                 if ($this->db->trans_status() === FALSE) {
                     $this->db->trans_rollback();
                     $vReturn->success = false;
@@ -90,16 +97,15 @@ class Fule extends PCenter {
                 }
             }
         }
-
         echo json_encode($vReturn);
     }
-
-    public function removeAccount() {
+    
+    public function removeMenu() {
         $_data = json_decode($_POST['data']);
         $vReturn = (object) [];
         $this->db->trans_begin();
 
-        $this->db->where_in('RowKey', $_data)->delete('MSTPump');
+        $this->db->where_in('RowKey', $_data)->delete('USRMenu');
 
         if ($this->db->trans_status() === FALSE) {
             $this->db->trans_rollback();
@@ -112,9 +118,18 @@ class Fule extends PCenter {
         echo json_encode($vReturn);
     }
 
-    //**** Fuel
-    public function branchMain(){
-        $data['page'] = 'master/Fule/branch_main';
-        $this->load->view('layout/nav', $data);
+    public function findSubMenu() {
+        $qryMenu = $this->db->select('RowKey, Menu, Description')->get('USRSubMenu');
+        $arMenu = array();
+        foreach ($qryMenu->result() as $row) {
+            $_ar = array(
+                'key' => $row->RowKey,
+                'Menu' => $row->Menu,
+                'Description' => $row->Description
+            );
+            array_push($arMenu, $_ar);
+        }
+        echo json_encode($arMenu);
     }
+
 }
--- application/controllers/insurance.php
+++ application/controllers/insurance.php
@@ -3,51 +3,47 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 require __DIR__ . '/../core/PCenter.php';
 
-class Fule extends PCenter {
+class  insurance extends PCenter {
 
     public function __construct() {
         parent::__construct();
-//        $this->load->helper('url');
-        //$this->load-model('dbconnect');
     }
 
-    //**** Pump
     public function index() {
-        $data['page'] = 'master/Fule/fule_main';
-
-        $this->load->model('fule_model');
-        $this->fule_model->insert_into_fule();
-
+        $data['page'] = 'master/insurance/insurance_main';
         $this->load->view('layout/nav', $data);
     }
 
     public function edit() {
-
-        $this->load->view('master/Fule/fule_edit');
+        
+          $this->load->view('master/insurance/insurance_edit');
+    }
+  public function findTitle() {
+        $query = $this->db->select('RowKey, Title')->from('MSTTitle')->order_by('Title', 'DESC')->get();
+        echo json_encode($query->result_array());
     }
 
-    public function findPump() {
-        $query = $this->db->select('RowKey, Pump, PumpType, ')->from('MSTPump')->get();
+    public function findInsurance() {
+        $query = $this->db->select('RowKey, InsuranceName,')->get('MSTInsurance');
         $_array = array();
         foreach ($query->result() as $row) {
             $_ar = array(
                 'key' => $row->RowKey,
-                'Pump' => $row->Pump,
-                'PumpType' => intval($row->PumpType),
+                'InsuranceName' => $row->InsuranceName,
             );
             array_push($_array, $_ar);
         }
         echo json_encode($_array);
     }
 
-    public function editPump() {
+     public function editinsurance() {
         $_data = json_decode($_POST['data']);
         $vReturn = (object) [];
 
         $this->db->trans_begin();
 
         if ($_data->RowKey === PCenter::GUID_EMPTY()) {
-            $queryChk = $this->db->where('Pump', $_data->Pump)->from('MSTPump')->count_all_results();
+            $queryChk = $this->db->where('InsuranceName', $_data->InsuranceName)->from('MSTInsurance')->count_all_results();
             if ($queryChk > 0) {
                 $vReturn->success = false;
                 $vReturn->message = 'This information is already in the system.';
@@ -58,7 +54,7 @@ class Fule extends PCenter {
                 $_data->CreateDate = PCenter::DATATIME_DB(new DateTime());
                 $_data->UpdateBy = PCenter::GUID_EMPTY();
                 $_data->UpdateDate = PCenter::DATATIME_DB(new DateTime());
-                $this->db->insert('MSTPump', $_data);
+                $this->db->insert('MSTInsurance', $_data);
                 if ($this->db->trans_status() === FALSE) {
                     $this->db->trans_rollback();
                     $vReturn->success = false;
@@ -69,17 +65,16 @@ class Fule extends PCenter {
                 }
             }
         } else {
-            $queryChk = $this->db->where('Pump', $_data->Pump)->where('RowKey !=', $_data->RowKey)->from('MSTPump')->count_all_results();
+            $queryChk = $this->db->where('InsuranceName', $_data->InsuranceName)->where('InsuranceName !=', $_data->InsuranceName)->from('MSTInsurance')->count_all_results();
             if ($queryChk > 0) {
                 $vReturn->success = false;
                 $vReturn->message = 'This information is already in the system.';
             } else {
                 $update = (object) [];
-                $update->Pump = $_data->Pump;
-                $update->PumpType = $_data->PumpType;
+                $update->InsuranceName = $_data->InsuranceName;
                 $update->UpdateBy = PCenter::GUID_EMPTY();
                 $update->UpdateDate = PCenter::DATATIME_DB(new DateTime());
-                $this->db->where('RowKey', $_data->RowKey)->update('MSTPump', $update);
+                $this->db->where('RowKey', $_data->RowKey)->update('MSTInsurance', $update);
                 if ($this->db->trans_status() === FALSE) {
                     $this->db->trans_rollback();
                     $vReturn->success = false;
@@ -94,12 +89,12 @@ class Fule extends PCenter {
         echo json_encode($vReturn);
     }
 
-    public function removeAccount() {
+    public function removeinsurance() {
         $_data = json_decode($_POST['data']);
         $vReturn = (object) [];
         $this->db->trans_begin();
 
-        $this->db->where_in('RowKey', $_data)->delete('MSTPump');
+        $this->db->where_in('RowKey', $_data)->delete('MSTInsurance');
 
         if ($this->db->trans_status() === FALSE) {
             $this->db->trans_rollback();
@@ -112,9 +107,4 @@ class Fule extends PCenter {
         echo json_encode($vReturn);
     }
 
-    //**** Fuel
-    public function branchMain(){
-        $data['page'] = 'master/Fule/branch_main';
-        $this->load->view('layout/nav', $data);
-    }
 }
