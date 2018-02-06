2016c294413b9941b704fb59d89eb3484ba751d3
Yongyuth (Private) <yongyuth_2527@hotmail.com>
2018-02-06 09:11:05 +0700

    Add DataTable Bootstrap

--- application/views/master/Admin/admin_edit.php
+++ application/views/master/Admin/admin_edit.php
@@ -8,71 +8,73 @@ and open the template in the editor.
     <h1>ข้อมูลAdmin</h1><h4><small>เพิ่มข้อมูลAdmin</small></h4>
 </div>-->
 <?php echo js_asset('views/admin/admin_edit.js') ?>
-<div class="row">
-    <form id="form_adminedit">
-        <div class="col-xs-12">
-            <div class="form-group">
-                <label for="txtUser">User Name :</label>
-                <div class="input-group">
-                    <div class="input-group-addon"><i class="fa fa-user-circle" style="min-width: 20px;"></i></div>
-                    <input type="text" class="form-control" id="txtUser" name="txtUser" placeholder="Input User Name">
+<div class="panel panel-default">
+    <div class="panel-body">
+        <form id="form_adminedit">
+            <div class="col-xs-12">
+                <div class="form-group">
+                    <label for="txtUser">User Name :</label>
+                    <div class="input-group">
+                        <div class="input-group-addon"><i class="fa fa-user-circle" style="min-width: 20px;"></i></div>
+                        <input type="text" class="form-control" id="txtUser" name="txtUser" placeholder="Input User Name">
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-xs-12">
-            <div class="form-group">
-                <label for="cmdTitle">Title :</label>
-                <select id="cmdTitle" name="cmdTitle" class="form-control selectpicker show-menu-arrow"
-                        data-width="100%"
-                        data-show-Tick="true"
-                        data-tick-Icon="fa fa-check"
-                        data-size="5"
-                        data-header="false"
-                        data-live-Search="true"
-                        data-live-Search-Placeholder="key word"
-                        data-multiple-Separator=",&nbsp;&nbsp;"
-                        data-actions-Box="false"
-                        data-selectAll-Text="Select All"
-                        data-deselectAll-Text="Deselect All"
-                        data-selected-Text-Format="count > 3">
-                </select>
-            </div>
-        </div>
-        <div class="col-xs-12">
-            <div class="form-group">
-                <label for="txtFirstName">First Name :</label>
-                <div class="input-group">
-                    <div class="input-group-addon"><i class="fa fa-vcard" style="min-width: 20px;"></i></div>
-                    <input type="text" class="form-control" id="txtFirstName" name="txtFirstName" placeholder="Input First Name">
+            <div class="col-xs-12">
+                <div class="form-group">
+                    <label for="cmdTitle">Title :</label>
+                    <select id="cmdTitle" name="cmdTitle" class="form-control selectpicker show-menu-arrow"
+                            data-width="100%"
+                            data-show-Tick="true"
+                            data-tick-Icon="fa fa-check"
+                            data-size="5"
+                            data-header="false"
+                            data-live-Search="true"
+                            data-live-Search-Placeholder="key word"
+                            data-multiple-Separator=",&nbsp;&nbsp;"
+                            data-actions-Box="false"
+                            data-selectAll-Text="Select All"
+                            data-deselectAll-Text="Deselect All"
+                            data-selected-Text-Format="count > 3">
+                    </select>
                 </div>
             </div>
-        </div>
-        <div class="col-xs-12">
-            <div class="form-group">
-                <label for="txtLastName">Last Name :</label>
-                <div class="input-group">
-                    <div class="input-group-addon"><i class="fa fa-vcard" style="min-width: 20px;"></i></div>
-                    <input type="text" class="form-control" id="txtLastName" name="txtLastName" placeholder="Input Last Name">
+            <div class="col-xs-12">
+                <div class="form-group">
+                    <label for="txtFirstName">First Name :</label>
+                    <div class="input-group">
+                        <div class="input-group-addon"><i class="fa fa-vcard" style="min-width: 20px;"></i></div>
+                        <input type="text" class="form-control" id="txtFirstName" name="txtFirstName" placeholder="Input First Name">
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-xs-12 showinadd">
-            <div class="form-group">
-                <label for="txtPassword">Password :</label>
-                <div class="input-group">
-                    <div class="input-group-addon"><i class="fa fa-ellipsis-h" style="min-width: 20px;"></i></div>
-                    <input type="password" class="form-control" id="txtPassword" name="txtPassword" placeholder="Input Password">
+            <div class="col-xs-12">
+                <div class="form-group">
+                    <label for="txtLastName">Last Name :</label>
+                    <div class="input-group">
+                        <div class="input-group-addon"><i class="fa fa-vcard" style="min-width: 20px;"></i></div>
+                        <input type="text" class="form-control" id="txtLastName" name="txtLastName" placeholder="Input Last Name">
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-xs-12 showinadd">
-            <div class="form-group">
-                <label for="txtConfirmPassword">Confirm Password :</label>
-                <div class="input-group">
-                    <div class="input-group-addon"><i class="fa fa-ellipsis-h" style="min-width: 20px;"></i></div>
-                    <input type="password" class="form-control" id="txtConfirmPassword" name="txtConfirmPassword" placeholder="Input Confirm Password">
+            <div class="col-xs-12 showinadd">
+                <div class="form-group">
+                    <label for="txtPassword">Password :</label>
+                    <div class="input-group">
+                        <div class="input-group-addon"><i class="fa fa-ellipsis-h" style="min-width: 20px;"></i></div>
+                        <input type="password" class="form-control" id="txtPassword" name="txtPassword" placeholder="Input Password">
+                    </div>
                 </div>
             </div>
-        </div>
-    </form>
+            <div class="col-xs-12 showinadd">
+                <div class="form-group">
+                    <label for="txtConfirmPassword">Confirm Password :</label>
+                    <div class="input-group">
+                        <div class="input-group-addon"><i class="fa fa-ellipsis-h" style="min-width: 20px;"></i></div>
+                        <input type="password" class="form-control" id="txtConfirmPassword" name="txtConfirmPassword" placeholder="Input Confirm Password">
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
 </div>
