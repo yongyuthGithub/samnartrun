4ae3a841051c6a9c29f8b9e146f5ebf2182a0a3b
Yongyuth (Private) <yongyuth_2527@hotmail.com>
2018-02-06 09:34:31 +0700

    Add DataTable Bootstrap

--- application/views/master/insurance/insurance_edit.php
+++ application/views/master/insurance/insurance_edit.php
@@ -8,16 +8,18 @@ and open the template in the editor.
     <h1>ข้อมูลAdmin</h1><h4><small>เพิ่มข้อมูลAdmin</small></h4>
 </div>-->
 <?php echo js_asset('views/insurance/insurance_edit.js') ?>
-<div class="row">
-    <form id="form_insuranceedit">
-        <div class="col-xs-12">
-            <div class="form-group">
-                <label for="txtUser">ชื่อบริษัทประกัน :</label>
-                <div class="input-group">
-                    <div class="input-group-addon"><i class="fa fa-user-circle" style="min-width: 20px;"></i></div>
-                    <input type="text" class="form-control" id="txtinsurance" name="txtinsurance" placeholder="ชื่อบริษัทประกัน">
+<div class="panel panel-default">
+    <div class="panel-body">
+        <form id="form_insuranceedit">
+            <div class="col-xs-12">
+                <div class="form-group">
+                    <label for="txtUser">ชื่อบริษัทประกัน :</label>
+                    <div class="input-group">
+                        <div class="input-group-addon"><i class="fa fa-user-circle" style="min-width: 20px;"></i></div>
+                        <input type="text" class="form-control" id="txtinsurance" name="txtinsurance" placeholder="ชื่อบริษัทประกัน">
+                    </div>
                 </div>
             </div>
-        </div>
-    </form>
+        </form>
+    </div>
 </div>
--- application/views/master/insurance/insurance_main.php
+++ application/views/master/insurance/insurance_main.php
@@ -4,11 +4,19 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<?php echo css_asset('views/admin/admin_main.css')?>
+<?php echo css_asset('views/admin/admin_main.css') ?>
 <?php echo js_asset('views/insurance/insurance_main.js') ?>
-<div class="page-header">
-    <h1>ข้อมูลประกัน</h1><h4><small>ข้อมูลบริษัทประกัน</small></h4>
+<div class="col-12">   
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <h1>ข้อมูลประกัน</h1><h4><small>ข้อมูลบริษัทประกัน</small></h4>
+        </div>
+    </div>
 </div>
 <div class="col-12">   
-    <div id="form_insurance"></div>
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <div id="form_insurance"></div>
+        </div>
+    </div>
 </div>
