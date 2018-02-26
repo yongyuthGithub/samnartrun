04328c64f6469ef218580875e75fe38bd8a67a30
Yongyuth (Private) <yongyuth_2527@hotmail.com>
2018-02-26 11:08:12 +0700

    Add Pemission Function

--- application/views/layout/nav.php
+++ application/views/layout/nav.php
@@ -5,14 +5,14 @@
         <title>บริษัท พราว</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!--        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">-->
-        <?php echo css_asset('master/bootstrap.min.css') ?>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <?php // echo css_asset('master/bootstrap.min.css') ?>
         <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
-<!--        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>-->
-        <?php echo js_asset('master/jquery.min.js') ?>
-<!--        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
-        <?php echo js_asset('master/bootstrap.min.js') ?>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <?php // echo js_asset('master/jquery.min.js') ?>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <?php // echo js_asset('master/bootstrap.min.js') ?>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <?php // echo css_asset('master/font-awesome.min.css') ?>
         <?php echo css_asset('master/_site.css') ?>
