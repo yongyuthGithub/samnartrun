94ba541362d74180c508120b8692fce06314d314
Yongyuth (Private) <yongyuth_2527@hotmail.com>
2018-04-02 15:26:40 +0700

    Add Pemission Function

--- application/config/config.php
+++ application/config/config.php
@@ -24,6 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] ='http://'. $_SERVER['HTTP_HOST']. '/samnartrun/';
+//$config['base_url'] ='http://'. $_SERVER['HTTP_HOST']. '/backoffice/';
 
 /*
 |--------------------------------------------------------------------------
